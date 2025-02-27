3bbfff8fc8750c0b571244f6c9523a3edffdc1c6
Michael <mbryanmandey@gmail.com>
2025-02-27 23:47:09 +0700

    5 commit

--- includes/footer.php
+++ includes/footer.php
@@ -32,6 +32,7 @@
     </footer>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
             <script src="/ssipfix/assets/js/main.js"></script>
+            <script src="/ssipfix/assets/js/voice-recorder.js"></script>
             
 </body>
 </html>
